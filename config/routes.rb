211cec8288d76f9f3a28ae6cc14da6ff4bff4301
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-12-24 00:37:10 -0800

    Temporarily disable trips#show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :bus_routes do
-    resources :trips
-  end
+  resources :bus_routes
   root to: 'welcome#index'
   get "connect" => "welcome#connect_to_actransit", :as => "connect_to_actransit"
   get "refresh" => "bus_routes#refresh_bus_routes", :as => "refresh_bus_routes"
